c9f8fb19a651bf6b088ac59d602aff8924c2cff8
DaniPM99 <dparramartin99@gmail.com>
2025-09-09 22:00:04 +0200

    Initial Commit

---
+++ HgDeveloperTest/HgDeveloperTestTests/HgDeveloperTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  HgDeveloperTestTests.swift
+//  HgDeveloperTestTests
+//
+//  Created by Daniel Parra Martin on 9/9/25.
+//
+
+import Testing
+@testable import HgDeveloperTest
+
+struct HgDeveloperTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
