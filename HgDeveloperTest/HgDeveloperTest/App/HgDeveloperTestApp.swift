7024c0ac0807c041e6f630521ef17a1fe2806dcf
DaniPM99 <dparramartin99@gmail.com>
2025-09-09 23:19:31 +0200

    Add Resource Videos, Images, Fonts and Colors

---
+++ HgDeveloperTest/HgDeveloperTest/App/HgDeveloperTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HgDeveloperTestApp.swift
+//  HgDeveloperTest
+//
+//  Created by Daniel Parra Martin on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HgDeveloperTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HgDeveloperTest/HgDeveloperTest/Core/Utils/Font+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Font+Extension.swift
+//  HgDeveloperTest
+//
+//  Created by Daniel Parra Martin on 9/9/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let hgTitle: Font = .custom("BasierCircle-Regular", size: 24)
+}
